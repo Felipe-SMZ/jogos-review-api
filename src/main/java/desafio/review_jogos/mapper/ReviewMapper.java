9847354f6640860c56561ee704e4516202b92c1e
Felipe <felipeshimizu5@gmail.com>
2026-05-01 18:42:05 -0300

    feat: ReviewService e ReviewRepository

--- src/main/java/desafio/review_jogos/mapper/ReviewMapper.java
+++ src/main/java/desafio/review_jogos/mapper/ReviewMapper.java
@@ -1,5 +1,6 @@
 package desafio.review_jogos.mapper;
 
+import desafio.review_jogos.dto.ReviewRequestDto;
 import desafio.review_jogos.dto.ReviewResponseDto;
 import desafio.review_jogos.model.Review;
 
@@ -13,5 +14,16 @@ public class ReviewMapper {
                 review.getComentario(),
                 review.getJogo() != null ? review.getJogo().getId() : null // Pega o ID do jogo
         );
+    }
+
+    public static Review toEntity(ReviewRequestDto dto) {
+        if (dto == null) return null;
+
+        Review review = new Review();
+        review.setId(dto.id());
+        review.setNota(dto.nota());
+        review.setComentario(dto.comentario());
+        // O Jogo é setado no Service, pois ele vem do banco
+        return review;
     }
 }
---
+++ src/main/java/desafio/review_jogos/service/ReviewService.java
@@ -0,0 +1,54 @@
+package desafio.review_jogos.service;
+
+import desafio.review_jogos.dto.MediaNotasResponseDto;
+import desafio.review_jogos.dto.ReviewRequestDto;
+import desafio.review_jogos.dto.ReviewResponseDto;
+import desafio.review_jogos.mapper.ReviewMapper;
+import desafio.review_jogos.model.Jogo;
+import desafio.review_jogos.model.Review;
+import desafio.review_jogos.repository.JogoRepository;
+import desafio.review_jogos.repository.ReviewRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReviewService {
+    private final ReviewRepository reviewRepository;
+    private final JogoRepository jogoRepository;
+
+    public ReviewService(ReviewRepository reviewRepository, JogoRepository jogoRepository) {
+        this.reviewRepository = reviewRepository;
+        this.jogoRepository = jogoRepository;
+    }
+
+    public ReviewResponseDto salvar(Long jogoId, ReviewRequestDto dto) {
+        Jogo jogo = jogoRepository.findById(jogoId)
+                .orElseThrow(() -> new RuntimeException("Jogo não encontrado"));
+
+        Review review = ReviewMapper.toEntity(dto);
+        review.setJogo(jogo); // Associa o jogo encontrado
+
+        Review reviewSalva = reviewRepository.save(review);
+
+        return ReviewMapper.toResponse(reviewSalva);
+    }
+
+    public List<ReviewResponseDto> listar(Long jogoId) {
+        if (!jogoRepository.existsById(jogoId)) {
+            throw new RuntimeException("Não foi possível listar reviews: Jogo não encontrado.");
+        }
+
+        return reviewRepository.findByJogoId(jogoId)
+                .stream()
+                .map(ReviewMapper::toResponse)
+                .toList();
+    }
+
+    public void deletar(Long id) {
+        Review reviewExiste = reviewRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Não foi possível excluir: Review com id " + id + " não encontrado."));
+        reviewRepository.deleteById(id);
+    }
+
+}
