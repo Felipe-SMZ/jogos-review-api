1b576512309565ff6a92da287960ccc2169e9060
Felipe <felipeshimizu5@gmail.com>
2026-05-01 18:41:35 -0300

    feat: JogoController

--- src/main/java/desafio/review_jogos/controller/JogoController.java
+++ src/main/java/desafio/review_jogos/controller/JogoController.java
@@ -1,9 +1,16 @@
 package desafio.review_jogos.controller;
 
 import desafio.review_jogos.dto.JogoResponseDto;
+import desafio.review_jogos.dto.MediaNotasResponseDto;
+import desafio.review_jogos.dto.ReviewRequestDto;
+import desafio.review_jogos.dto.ReviewResponseDto;
 import desafio.review_jogos.mapper.JogoMapper;
+import desafio.review_jogos.mapper.ReviewMapper;
 import desafio.review_jogos.model.Jogo;
+import desafio.review_jogos.model.Review;
 import desafio.review_jogos.service.JogoService;
+import desafio.review_jogos.service.ReviewService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +22,11 @@ import java.util.List;
 public class JogoController {
 
     private final JogoService jogoService;
+    private final ReviewService reviewService;
 
-    public JogoController(JogoService jogoService) {
+    public JogoController(JogoService jogoService, ReviewService reviewService) {
         this.jogoService = jogoService;
+        this.reviewService = reviewService;
     }
 
     @PostMapping
@@ -40,6 +49,28 @@ public class JogoController {
     public ResponseEntity<Void> remover(@PathVariable Long id) {
         jogoService.excluir(id);
         return ResponseEntity.noContent().build();
+    }
+
+    //Reviews do jogo
+    @PostMapping("/{id}/reviews")
+    public ResponseEntity<ReviewResponseDto> inserirReview(
+            @PathVariable Long id,
+            @Validated @RequestBody ReviewRequestDto dto) {
+
+        // Passamos o ID da URL e o DTO para o Service
+        ReviewResponseDto response = reviewService.salvar(id, dto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{id}/reviews")
+    public ResponseEntity<List<ReviewResponseDto>> buscarTodosReviewsPorJogo(@PathVariable Long id) {
+        return ResponseEntity.ok(reviewService.listar(id));
+    }
+
+    @GetMapping("/{id}/media")
+    public ResponseEntity<MediaNotasResponseDto> mediaNotaPorJogo(@PathVariable Long id) {
+        return ResponseEntity.ok(jogoService.buscarMediaDoJogo(id));
     }
 }
 
