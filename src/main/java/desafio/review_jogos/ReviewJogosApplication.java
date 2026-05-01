9e1e35978b11a6605b727d69c206a55b6fa68672
Felipe <felipeshimizu5@gmail.com>
2026-05-01 13:43:26 -0300

    docs: exemplo do aplication properties

---
+++ src/main/java/desafio/review_jogos/ReviewJogosApplication.java
@@ -0,0 +1,13 @@
+package desafio.review_jogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewJogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewJogosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/desafio/review_jogos/ReviewJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.review_jogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewJogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
