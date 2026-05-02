3eac9000c1a7df49468d30c09970dce86ef81787
Felipe <felipeshimizu5@gmail.com>
2026-05-02 05:37:02 -0300

    doc: arrumando o swagger para enviar os parametros corretos

--- src/main/java/desafio/review_jogos/repository/JogoRepository.java
+++ src/main/java/desafio/review_jogos/repository/JogoRepository.java
@@ -2,10 +2,10 @@ package desafio.review_jogos.repository;
 
 import desafio.review_jogos.model.Jogo;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
 
 @Repository
 public interface JogoRepository extends JpaRepository<Jogo, Long> {
