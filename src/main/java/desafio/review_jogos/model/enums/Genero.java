e6a763740d39867300ff3983fbea8f132dcee8f5
Felipe <felipeshimizu5@gmail.com>
2026-05-02 06:45:39 -0300

    refactor: Genero foi definida como enum

---
+++ src/main/java/desafio/review_jogos/model/enums/Genero.java
@@ -0,0 +1,24 @@
+package desafio.review_jogos.model.enums;
+
+public enum Genero {
+    ACAO,
+    AVENTURA,
+    RPG,
+    ESTRATEGIA,
+    ESPORTES,
+    CORRIDA,
+    LUTA,
+    FPS,
+    TPS,
+    SURVIVAL,
+    HORROR,
+    PLATAFORMA,
+    METROIDVANIA,
+    ROGUELIKE,
+    SIMULACAO,
+    PUZZLE,
+    STEALTH,
+    MUSICAL,
+    VISUAL_NOVEL,
+    MOBILE
+}
